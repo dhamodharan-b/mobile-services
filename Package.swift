d4ec8730e706867752b01bdcf3e9050c4e4a6199
AC49912 <ac49912@IND-QYRY7Y.local>
2022-02-23 18:22:56 +0530

    update removed product

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 let package = Package(
     name: "AdobeMobileLibrary",
     platforms: [.iOS(.v9)],
-    products: [
-        .library(
-            name: "AdobeMobileLibrary",
-            targets: ["AdobeMobileLibrary"]
-        )
-    ],
     targets: [
         .target(
             name: "AdobeMobileLibrary",
