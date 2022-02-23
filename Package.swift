e5312a66bb4e5e06629ae0869b991e516d043d3f
AC49912 <ac49912@IND-QYRY7Y.local>
2022-02-23 18:48:32 +0530

    update

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 let package = Package(
     name: "mobile-services",
     platforms: [.iOS(.v9)],
-    products: [
-        .library(
-            name: "ADBMobile",
-            targets: ["ADBMobile"]
-        )
-    ],
     targets: [
         .target(
             name: "ADBMobile",
