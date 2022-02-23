fcd58563e65fe616320ea8c563904690018477e2
AC49912 <ac49912@IND-QYRY7Y.local>
2022-02-23 18:59:22 +0530

    updated

--- Package.swift
+++ Package.swift
@@ -4,14 +4,15 @@ import PackageDescription
 let package = Package(
     name: "mobile-services",
     platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "mobile-services",
+            targets: ["mobile-services"]
+        )
+    ],
     targets: [
         .target(
-            name: "ADBMobile",
-            path: "Sources/AdobeMobileLibrary",
-            publicHeadersPath: "",
-            cSettings: [
-              .headerSearchPath("include"),
-            ]
+            name: "mobile-services"
         )
     ]
 )
