89bf220b468d13c87fba15847cd627127bcaaf87
AC49912 <ac49912@IND-QYRY7Y.local>
2022-02-23 19:32:19 +0530

    update

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "mobile-services",
-            targets: ["AdobeMobile"]),
+            targets: ["mobile-services"]),
     ],
     dependencies: [],
     targets: [
