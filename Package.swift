4d4e3e213a30c527c2dbf664feb35eca787eae0a
AC49912 <ac49912@IND-QYRY7Y.local>
2022-02-23 19:33:56 +0530

    update

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 let package = Package(
     name: "mobile-services",
     platforms: [.iOS(.v9)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "mobile-services",
-            targets: ["mobile-services"]),
-    ],
     dependencies: [],
     targets: [
         .binaryTarget(name: "AdobeMobile", path: "./Sources/AdobeMobile.xcframework")
