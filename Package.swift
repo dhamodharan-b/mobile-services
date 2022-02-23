8e0464cadca41895be571449b3bac2ed5bdd17b6
AC49912 <ac49912@IND-QYRY7Y.local>
2022-02-23 20:08:57 +0530

    update

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "AdobeMobileSDK",
             path: "Sources/AdobeMobileSDK",
             publicHeadersPath: ""
